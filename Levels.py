733067c022145472f1d96f4656ba73348dd12eaa
ferijen <150139765+ferijen@users.noreply.github.com>
2024-03-06 12:52:17 +0100

    Update Levels.py

--- Levels.py
+++ Levels.py
@@ -6,9 +6,9 @@ class Levels:
         self.selected_level = None
         self.levels = ["Easy", "Medium", "Hard"]
         self.level_difficulties = {
-            "Easy": {"speed": 10, "grid_size": 35, "background": "grass.png"},
-            "Medium": {"speed": 15, "grid_size": 25, "background": "rocks.png"},
-            "Hard": {"speed": 20, "grid_size": 20, "background": "space.png"}
+            "Easy": {"speed": 10, "GRID_SIZE": 35, "background": "grass.png"},
+            "Medium": {"speed": 15, "GRID_SIZE": 25, "background": "rocks.png"},
+            "Hard": {"speed": 20, "GRID_SIZE": 20, "background": "space.png"}
         }
         self.font = pygame.font.Font(None, 36)
         self.text_color = (0, 0, 255)
