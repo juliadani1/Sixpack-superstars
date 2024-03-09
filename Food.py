88564b8a5d3ee547b92a80830ed4d9de0e6f05fc
ferijen <150139765+ferijen@users.noreply.github.com>
2024-03-09 10:28:35 +0100

    Update Food.py

--- Food.py
+++ Food.py
@@ -3,13 +3,16 @@ import random
 from constants import GRID_WIDTH, GRID_HEIGHT, GRIDSIZE
 
 class Food:
+    # Skapar maten
     def __init__(self):
         self.position = (0, 0)
         self.color = (220, 20, 60) 
         self.random_plats()
 
+    # Slumpar en plats för maten på skärmen
     def random_plats(self):
         self.position = (random.randint(0, GRID_WIDTH-1)*GRIDSIZE, random.randint(0, GRID_HEIGHT-1)*GRIDSIZE)
 
+    # Ritar upp maten på skärmen
     def måla(self, surface):
         pygame.draw.rect(surface, self.color, (self.position[0], self.position[1], GRIDSIZE, GRIDSIZE))
