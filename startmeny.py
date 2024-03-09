ff9a0dd3fa266701e854a05623a9b373fb6dcb6d
ferijen <150139765+ferijen@users.noreply.github.com>
2024-03-09 10:21:34 +0100

    Update startmeny.py

--- startmeny.py
+++ startmeny.py
@@ -1,15 +1,15 @@
 import pygame
 import sys
  
-def display_menu():
+def visa_meny():
     pygame.init()
 
     screen = pygame.display.set_mode((700, 700))
     font = pygame.font.Font(None, 36)
 
-    button1 = pygame.Rect(250, 100, 200, 50)  # x, y, width, height
-    button2 = pygame.Rect(250, 200, 200, 50)
-    button3 = pygame.Rect(250, 300, 200, 50)
+    knapp1 = pygame.Rect(250, 100, 200, 50)  # x, y, width, height
+    knapp2 = pygame.Rect(250, 200, 200, 50)
+    knapp3 = pygame.Rect(250, 300, 200, 50)
 
     while True:
         for event in pygame.event.get():
@@ -20,18 +20,18 @@ def display_menu():
                 mouse_pos = event.pos  # gets mouse position
 
                 # checks if mouse position is over the button
-                if button1.collidepoint(mouse_pos):
+                if knapp1.collidepoint(mouse_pos):
                     return 1
-                elif button2.collidepoint(mouse_pos):
+                elif knapp2.collidepoint(mouse_pos):
                     return 2
-                elif button3.collidepoint(mouse_pos):
+                elif knapp3.collidepoint(mouse_pos):
                     return 3
 
         screen.fill((183, 225, 238))
 
-        pygame.draw.rect(screen, [253, 253, 150], button1)  # draw button
-        pygame.draw.rect(screen, [237, 110, 0], button2)  # draw button
-        pygame.draw.rect(screen, [210, 1, 23], button3)  # draw button
+        pygame.draw.rect(screen, [253, 253, 150], knapp1)  # draw button
+        pygame.draw.rect(screen, [237, 110, 0], knapp2)  # draw button
+        pygame.draw.rect(screen, [210, 1, 23], knapp3)  # draw button
 
         text1 = font.render('Level 1', True, (0, 0, 0))
         screen.blit(text1, (300, 110))
@@ -44,5 +44,3 @@ def display_menu():
 
         pygame.display.flip()
     
-
-level = display_menu()
