99d57f5d3ec4204edf121f6818a3382cee83d4e7
ferijen <150139765+ferijen@users.noreply.github.com>
2024-03-09 10:08:23 +0100

    Update Food.py

--- Food.py
+++ Food.py
@@ -6,10 +6,10 @@ class Food:
     def __init__(self):
         self.position = (0, 0)
         self.color = (220, 20, 60) 
-        self.randomize_position()
+        self.random_plats()
 
-    def randomize_position(self):
+    def random_plats(self):
         self.position = (random.randint(0, GRID_WIDTH-1)*GRIDSIZE, random.randint(0, GRID_HEIGHT-1)*GRIDSIZE)
 
-    def draw(self, surface):
+    def måla(self, surface):
         pygame.draw.rect(surface, self.color, (self.position[0], self.position[1], GRIDSIZE, GRIDSIZE))
