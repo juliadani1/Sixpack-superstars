820286d0bbbc755132e6fa06fa31388da2d61db9
ferijen <150139765+ferijen@users.noreply.github.com>
2024-03-09 10:50:29 +0100

    Update startmeny.py

--- startmeny.py
+++ startmeny.py
@@ -1,12 +1,11 @@
 import pygame
 import sys
- 
+
+# Visar upp menyn med grafiska element
 def visa_meny():
     pygame.init()
-
     screen = pygame.display.set_mode((700, 700))
     font = pygame.font.Font(None, 36)
-
     knapp1 = pygame.Rect(250, 100, 200, 50)  # x, y, width, height
     knapp2 = pygame.Rect(250, 200, 200, 50)
     knapp3 = pygame.Rect(250, 300, 200, 50)
@@ -19,7 +18,7 @@ def visa_meny():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos  # gets mouse position
 
-                # checks if mouse position is over the button
+                # Kontrollerar om musen klickar på knapp
                 if knapp1.collidepoint(mouse_pos):
                     return 1
                 elif knapp2.collidepoint(mouse_pos):
@@ -27,18 +26,16 @@ def visa_meny():
                 elif knapp3.collidepoint(mouse_pos):
                     return 3
 
+       # Målar upp backgrund och knapparna
         screen.fill((183, 225, 238))
-
-        pygame.draw.rect(screen, [253, 253, 150], knapp1)  # draw button
-        pygame.draw.rect(screen, [237, 110, 0], knapp2)  # draw button
-        pygame.draw.rect(screen, [210, 1, 23], knapp3)  # draw button
+        pygame.draw.rect(screen, [253, 253, 150], knapp1)  
+        pygame.draw.rect(screen, [237, 110, 0], knapp2) 
+        pygame.draw.rect(screen, [210, 1, 23], knapp3)  
 
         text1 = font.render('Level 1', True, (0, 0, 0))
         screen.blit(text1, (300, 110))
-
         text2 = font.render('Level 2', True, (0, 0, 0))
         screen.blit(text2, (300, 210))
-
         text3 = font.render('Level 3', True, (0, 0, 0))
         screen.blit(text3, (300, 310))
 
