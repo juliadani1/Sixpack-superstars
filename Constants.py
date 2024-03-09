e58e56132169b8449c8ebeb95faf35acaffe5bc2
ferijen <150139765+ferijen@users.noreply.github.com>
2024-03-09 10:26:39 +0100

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,10 +1,13 @@
+# Sätter storlek på skärmen
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 700
 
+# Storlek på rutorna i rutnätet och motsvarar antalet rutor som passar in i skärmen
 GRIDSIZE = 35
 SCREEN_WIDTH = SCREEN_WIDTH // GRIDSIZE
 SCREEN_HEIGHT = SCREEN_HEIGHT // GRIDSIZE
 
+# Koordinatsystem för riktningar
 UP = (0,-1)
 DOWN = (0,1)
 LEFT = (-1,0)
